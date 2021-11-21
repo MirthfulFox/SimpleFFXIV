5bafc165ce8e5b38ab1f4eefd48d5d935aae6e4a
Javier Fernández <javifdezsu@protonmail.com>
2021-11-21 18:43:03 +0100

    implemented xivsearch.py

---
+++ simpleffxiv/cogs/xivsearch.py
@@ -0,0 +1,28 @@
+from os import getenv
+from discord.ext import commands
+import pyxivapi
+
+class XIVSearch(commands.Cog):
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command(name="playersearch")
+    async def playersearch(self, ctx: commands.Context, *args):
+        if (len(args)!=3):
+            await ctx.send("Syntax is: "+ getenv("prefix") +"playersearch forename surname world")
+        else:
+            client = pyxivapi.XIVAPIClient(api_key=getenv("XIVAPIKey"))
+            character = await client.character_search(world=args[2], forename=args[0], surname=args[1])
+
+            if(character['Pagination']['Results'] == 1):
+                charID=(character['Results'][0]['ID'])
+                await ctx.send("https://eu.finalfantasyxiv.com/lodestone/character/"+str(charID))
+            else:
+                await ctx.send("Character not found")
+                
+            await client.session.close()
+        
+
+def setup(bot: commands.Bot):
+    bot.add_cog(XIVSearch(bot))
