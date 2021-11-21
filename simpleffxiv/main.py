2831fd9acf0054c0c0931385f046593d176ecf09
Javier Fernández <javifdezsu@protonmail.com>
2021-11-21 03:07:15 +0100

    Update main.py

--- simpleffxiv/main.py
+++ simpleffxiv/main.py
@@ -8,10 +8,6 @@ bot = commands.Bot(command_prefix="?")
 
 bot.load_extension("cogs.itemsearch")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
-
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game(getenv("BOT_STATUS")))  # Update Bot status
